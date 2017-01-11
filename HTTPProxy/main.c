fdba96f651253695a1153b10264dc66b4440c09d
MacPu <geniusgeek@163.com>
2017-01-11 11:12:03 +0800

    add version info in start log

--- HTTPProxy/main.c
+++ HTTPProxy/main.c
@@ -7,10 +7,29 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "LAHTTPProxy.h"
 
+#define Version "1.0"
+
 int main(int argc, const char * argv[]) {
-    printf("Hello, World!\n");
-    start_http_proxy(8088);
+    if(argc != 2){
+        printf("[HTTPProxy] cannot start proxy with invalied arg count\r\n");
+        return 0;
+    }
+    
+    const char *port_str = argv[1];
+    int port = atoi(port_str);
+    if(port == 0){
+        printf("[HTTPProxy] cannot start proxy with invalied port\r\n");
+        return 0;
+    }
+    
+    printf("****************************************\r\n");
+    printf("HTTPProxy (v %s)\r\n", Version);
+    printf("star on https://github.com/MacPu/HTTPProxy \r\n");
+    printf("****************************************\r\n\r\n");
+    
+    start_http_proxy(port);
     return 0;
 }
